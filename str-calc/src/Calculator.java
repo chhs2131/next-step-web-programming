dd108f7fadf895caddf5b60c96b70dc6be0f2b04
Basak <chgy2131@naver.com>
2023-01-14 22:01:12 +0900

    [feat] 계산기 객체 추가

---
+++ str-calc/src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    int add(int i, int j) {
+        return i + j;
+    }
+
+    int subtract(int i, int j) {
+        return i - j;
+    }
+
+    int multiply(int i, int j) {
+        return i * j;
+    }
+
+    int divide(int i, int j) {
+        return i / j;
+    }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(3, 4));
+    }
+}
